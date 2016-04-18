1e940338c98b6534792ac60ee156fb4445a7bbae
vladomelchenko <vladomelchenko98@gmail.com>
2016-04-18 21:19:34 +0300

    Added files via upload

--- index.php
+++ index.php
@@ -24,15 +24,7 @@
 		
         <?php
     session_start();
-    /*if ($_SESSION['log']!=null){
-        echo "
-<script>
-window.location='notes.php';
-</script>
-";
-    }
-    else
-    {*/
+    
 $log=$_GET['log'];
 $pass=$_GET['pass'];
 $type=$_GET['type'];
@@ -44,19 +36,6 @@ if ($content==null and $type=="Вход"){
     echo '<script>
     alert("Неправильный логин или пароль!");
     window.location="index.php";
-    </script>';
-    return false;
-    }
-if ($content==null and $type=="Регистрация"){
-    $users->insert($user);
-    echo '<script>
-    alert("Вы успешно зарегистрированы");
-    </script>';
-    return false;
-    }
-if ($content!=null and $type=="Регистрация"){
-    echo '<script>
-    alert("Такой логин уже используется!");
     </script>';
     return false;
     }
--- index1.php
+++ index1.php
@@ -24,15 +24,7 @@
 		
         <?php
     session_start();
-    /*if ($_SESSION['log']!=null){
-        echo "
-<script>
-window.location='notes.php';
-</script>
-";
-    }
-    else
-    {*/
+  
 $log=$_GET['log'];
 $pass=$_GET['pass'];
 $type=$_GET['type'];
@@ -40,13 +32,7 @@ $db=new MongoClient();
 $users=$db->notes->users;
 $user = array( "username" => $log, "password" => $pass); 
 $content=$users->findOne($user);
-if ($content==null and $type=="Вход"){
-    echo '<script>
-    alert("Неправильный логин или пароль!");
-    window.location="index.php";
-    </script>';
-    return false;
-    }
+
 if ($content==null and $type=="Регистрация"){
     $users->insert($user);
     echo '<script>
@@ -60,15 +46,7 @@ if ($content!=null and $type=="Регистрация"){
     </script>';
     return false;
     }
-if ($content!=null and $type=="Вход"){
-    echo '<script>
-    alert("Вы успешно зашли!");
-    window.location="notes.php";
-    </script>'; 
-        $_SESSION["log"] = $content["username"];
-    return true;
-//}
-}
+
 ?>
 </body>
 </html>
